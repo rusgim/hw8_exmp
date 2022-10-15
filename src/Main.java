e21f8566469273ba7fdb4bca07c5dcb0300a96f2
Ruslan <gimazevs@mail.ru>
2022-10-15 22:49:14 +0300

    первый коммит

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        //задание 1
+        int[] number = new int[3];
+        number[0] = 1;
+        number[1] = 2;
+        number[2] = 3;
+        System.out.print(number[0] + "," + number[1] + "," + number[2]);
+        System.out.println(" ");
+        double[] fractionalNumbers = new double[] {1.57, 7.654, 9.986};
+        System.out.print(fractionalNumbers[0] + "," + fractionalNumbers[1] + "," + fractionalNumbers[2]);
+        int[] random = {1, 78, 0};
+        System.out.println(random);
+        //задача 2
+        System.out.println(" ");
+
+    }
+}
